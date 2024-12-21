6d29d5d40da0803c04b34f06d5215ca250e8905a
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2024-12-21 08:10:57 +0530

    Uploading code: Sum of First N Natural Numbers.py

---
+++ Sum of First N Natural Numbers.py
@@ -0,0 +1,10 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args) 
+    {
+        Scanner r = new Scanner(System.in);
+        int n = r.nextInt();
+        int a = n*(n+1)/2;
+        System.out.println(a);
+    }
+}
