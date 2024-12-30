0192e419c02130b794f16f7f7b460a0736af529a
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2024-12-30 15:58:17 +0530

    Uploading code: Take Input And Print .py

---
+++ Take Input And Print .py
@@ -0,0 +1,8 @@
+import java.util.*;
+public class girigiri{
+    public static void main(String[] args){
+        Scanner r=new Scanner(System.in);
+        String n=r.nextLine();
+        System.out.println(n);
+    }
+}
