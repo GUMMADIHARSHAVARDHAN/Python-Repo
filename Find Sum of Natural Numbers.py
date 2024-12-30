79299e0e96d3890b91fd7c1108a9a780f8f66230
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2024-12-30 14:00:33 +0530

    Uploading code: Find Sum of Natural Numbers.py

---
+++ Find Sum of Natural Numbers.py
@@ -0,0 +1,8 @@
+import java.util.*;
+public class crazy{
+    public static void main(String[] args){
+        Scanner r = new Scanner(System.in);
+        int n = r.nextInt();
+        System.out.println(n*(n+1)/2);
+    }
+}
