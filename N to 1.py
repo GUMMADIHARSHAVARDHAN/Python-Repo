fda17b6398af8c07e3e1792aa0fbb9c41642460f
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2024-12-30 15:31:13 +0530

    Uploading code: N to 1.py

---
+++ N to 1.py
@@ -0,0 +1,11 @@
+import java.util.*;
+public class simba{
+    public static void main(String[] args){
+        Scanner r = new Scanner(System.in);
+        int n = r.nextInt();
+        while(n>0){
+            System.out.printf("%d ",n);
+            --n;
+        }
+    }
+}
