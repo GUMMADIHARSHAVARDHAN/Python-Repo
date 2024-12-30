2741e73ab3de2387bde0b932fff816bad41b6d45
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2024-12-30 15:08:16 +0530

    Uploading code: A to B.py

---
+++ A to B.py
@@ -0,0 +1,11 @@
+import java.util.*;
+public class zazu{
+    public static void main(String[] args){
+        Scanner r = new Scanner(System.in);
+        int a=r.nextInt();
+        int b =r.nextInt();
+        for(int i=a;i<=b;i++){
+            System.out.printf("%d ",i);
+        }
+    }
+}
